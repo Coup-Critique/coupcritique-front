da66adbf2fb5513fded172d637c6ecccefa1a5f1
GeoDaz <gfrydz2@gmail.com>
2024-09-13 18:24:42 +0200

    start

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;`;
+// const cspHeader = `
+//     default-src 'self';
+//     script-src 'self' 'unsafe-eval' 'unsafe-inline';
+//     style-src 'self' 'unsafe-inline';
+//     img-src 'self' blob: data:;
+//     font-src 'self';
+//     object-src 'none';
+//     base-uri 'self';
+//     form-action 'self';
+//     frame-ancestors 'none';
+//     upgrade-insecure-requests;`;
 
 const nextConfig = {
 	// output: 'export',
@@ -34,6 +34,10 @@ const nextConfig = {
 	// 		},
 	// 	];
 	// },
+	i18n: {
+		locales: ['en', 'fr'],
+		defaultLocale: 'fr',
+	},
 };
 
 module.exports = nextConfig;
